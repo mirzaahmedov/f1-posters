e2539d538d065f751eed2c25b7c408f6acf6aedf
mirzaahmedov.dev <mirzaahmedov.dev@gmail.com>
2023-01-04 16:02:07 +0500

    change to alfa romeo

--- src/views/Posters.vue
+++ src/views/Posters.vue
@@ -3,17 +3,53 @@ import { ref } from "vue";
 
 const posters = ref([
   {
-    grandPrix: "Abu Dhabi",
+    grandPrix: "Portuguese Grand Prix",
     season: "2021/2022",
     image:
-      "https://api.ferrari.com/cms/network/medias//resize/6374af45f6f433002330d390-2022-ferrari-abu-dhabi-grand-prix-cover-art-full?apikey=9QscUiwr5n0NhOuQb463QEKghPrVlpaF&width=768&height=0",
+      "https://www.sauber-group.com/wp-content/uploads/2021/05/pz-830x1475.jpg",
+  },
+  {
+    grandPrix: "San Paolo",
+    season: "2021/2022",
+    image:
+      "https://www.sauber-group.com/wp-content/uploads/2021/07/Baku_21_750x1334.jpg",
+  },
+  {
+    grandPrix: "Portuguese Grand Prix",
+    season: "2021/2022",
+    image:
+      "https://www.sauber-group.com/wp-content/uploads/2021/05/pz-830x1475.jpg",
+  },
+  {
+    grandPrix: "San Paolo",
+    season: "2021/2022",
+    image:
+      "https://www.sauber-group.com/wp-content/uploads/2021/07/Baku_21_750x1334.jpg",
   },
 ]);
 </script>
 
 <template>
-  <div v-for="poster in posters" class="poster">
-    <img :src="poster.image" />
-    <h1>{{ poster.grandPrix }}</h1>
+  <div class="grid">
+    <div v-for="poster in posters" class="poster">
+      <img :src="poster.image" />
+      <h1>{{ poster.grandPrix }}</h1>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+.poster {
+  cursor: pointer;
+}
+.poster h1 {
+  font-size: 1.3rem;
+}
+.poster img {
+  width: 100%;
+}
+</style>
